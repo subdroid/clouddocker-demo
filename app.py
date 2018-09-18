213aeb5ed045601c79141e51a03c7e6bf7cd7ddb
Norkhairi Misdi <norkhairi@gmail.com>
2018-09-18 15:36:17 +0800

    Update tetx

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_hit_count():
 
 def hello():
       count = get_hit_count()
-      return 'Hello World! I have been seen {} times.\n'.format(count)
+      return 'Hello World! I have been seen {} times. Okthxbye\n'.format(count)
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", debug=True)
